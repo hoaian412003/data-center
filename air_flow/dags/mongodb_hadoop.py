5ecea48616c3743ad6f924faed41ff0f6ffb1458
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2023-10-05 15:41:10 +0700

    Update put hdfs

--- air_flow/dags/mongodb_hadoop.py
+++ air_flow/dags/mongodb_hadoop.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import json
 
 # The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
@@ -9,65 +10,77 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.utils.dates import days_ago
 
 from airflow.providers.mongo.hooks.mongo import MongoHook, MongoClient
-from flask import jsonify
+from hdfs import InsecureClient
+from airflow.models.param import Param
+
 
 # DAG creation
 dag = DAG(
     "mongodb_hadoop",
     description="Move all data in mongodb base on your configuration to hadoop (HDFS) system DAG",
     start_date=days_ago(2),
-    default_args={
-        "conn_id": "PoolsWallet",
-        "mongo_db": "poolwallet",
-        "mongo_collection": "accesslogs",
-        "batch_size": 100,  # Default number of records will send to hdfs in one batch
+    params={
+        "conn_id": Param("PoolsWallet", type="string"),
+        "mongo_db": Param("poolwallet", type="string"),
+        "mongo_collection": Param("banners", type="string"),
+        "batch_size": Param(100, type="integer"),
+        "hdfs_uri": Param("http://namenode:9870", type="string"),
     },
-)
-
-hook = MongoHook(conn_id=dag.default_args["conn_id"])
-collection = hook.get_collection(
-    mongo_collection=dag.default_args["mongo_collection"],
-    mongo_db=dag.default_args["mongo_db"],
+    render_template_as_native_obj=True,
 )
 
 
-def getTotalRecords():
-    total = collection.count_documents(filter={})
-    return total
+def task(**context):
+    args = context["params"]
 
+    hook = MongoHook(conn_id=args["conn_id"])
+    collection = hook.get_collection(
+        mongo_collection=args["mongo_collection"],
+        mongo_db=args["mongo_db"],
+    )
+    hdfsClient = InsecureClient(args["hdfs_uri"], "root")
 
-def fetchBatchRecord(batchNumber: int):
-    batchSize = dag.default_args["batch_size"]
-    records = collection.find().skip(batchNumber * batchSize).limit(batchSize)
-    return records
+    def getTotalRecords():
+        total = collection.count_documents(filter={})
+        return total
 
+    def fetchBatchRecord(batchNumber: int):
+        batchSize = args["batch_size"]
+        records = collection.find().skip(batchNumber * batchSize).limit(batchSize)
+        return records
 
-def fetchAllRecords():
-    total = getTotalRecords()
-    batchCount = int(total / dag.default_args["batch_size"]) + 1
-    totalRecordProcessed = 0
+    def fetchAllRecords():
+        total = getTotalRecords()
+        batchCount = int(total / args["batch_size"]) + 1
+        totalRecordProcessed = 0
 
-    print("Total of batch / record is: ", batchCount, total)
+        print("Total of batch / record is: ", batchCount, total)
 
-    for bathNumber in range(batchCount):
-        print("Processing bath #", bathNumber)
-        records = fetchBatchRecord(bathNumber)
-        result = []
-        for record in records:
-            record["_id"] = str(record["_id"])
-            result.append(record)
-            totalRecordProcessed = totalRecordProcessed + 1
-        print("Found ", len(result), " records")
+        for bathNumber in range(batchCount):
+            print("Processing bath #", bathNumber)
+            records = fetchBatchRecord(bathNumber)
+            result = []
+            for record in records:
+                record["_id"] = str(record["_id"])
+                result.append(record)
+                totalRecordProcessed = totalRecordProcessed + 1
+            putDataToHadoop(result, args["mongo_collection"] + "/block_" + str(bathNumber))
+            print("Found ", len(result), " records")
 
-    return totalRecordProcessed
+        return totalRecordProcessed
 
+    def putDataToHadoop(data, name_file: str):
+        jsonData = json.dumps(data, default=str)
+        with hdfsClient.write(name_file) as writer:
+            writer.write(jsonData)
+        return jsonData
 
-def putDataToHadoop():
-    pass
+    # Main Runner
+    fetchAllRecords()
 
 
 t1 = PythonOperator(
     task_id="fetch",
     dag=dag,
-    python_callable=fetchAllRecords,
+    python_callable=task,
 )
